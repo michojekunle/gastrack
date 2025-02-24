9a94065c7d40284ba2e3081bc860e9a86247db08
AMD <michojekunle1@gmail.com>
2025-02-24 06:50:05 +0100

    Update index.js to validate file existence before processing

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ import path from "path";
 import ora from "ora";
 import inquirer from "inquirer";
 import { execSync } from "child_process";
+import { existsSync } from "fs";
 
 const program = new Command();
 
 program
   .version("1.0.0")
-  .argument("<project-name>", "GasTrack")
-  .action(async (projectName) => {});
+  .argument("<file-path>", "File Path")
+  .action(async (fileName) => {
+    const filePath = path.join(process.cwd(), fileName);
+
+    if(!fs.existsSync(filePath)) {
+      console.error(
+        chalk.red(`Error: File ${fileName} not found in current working directory!!`)
+      );
+
+      process.exit(1);
+    }
+
+    
+
+  });
